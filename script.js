cc770c3cd8fe98ba3c65e6108e31f8b969762da1
studypilotlucente <lucentecorporate@gmail.com>
2026-03-15 14:42:22 +1100

    new script.js

--- script.js
+++ script.js
@@ -1,7 +1,51 @@
-const buttons = document.querySelectorAll(".plan-btn");
 
-buttons.forEach((button) => {
-  button.addEventListener("click", () => {
-    alert("Plan selection feature coming soon!");
-  });
-});
+
+function saveNotes(){
+
+let notes=document.getElementById("notesInput").value
+
+let list=document.getElementById("notesList")
+
+let div=document.createElement("div")
+
+div.innerText=notes
+
+list.appendChild(div)
+
+}
+
+
+
+function addFlashcard(){
+
+let front=document.getElementById("flashFront").value
+
+let back=document.getElementById("flashBack").value
+
+let list=document.getElementById("flashcardsList")
+
+let card=document.createElement("div")
+
+card.innerHTML="<b>"+front+"</b><br>"+back
+
+list.appendChild(card)
+
+}
+
+
+
+function addQuiz(){
+
+let q=document.getElementById("quizQuestion").value
+
+let a=document.getElementById("quizAnswer").value
+
+let list=document.getElementById("quizList")
+
+let div=document.createElement("div")
+
+div.innerHTML=q+"<br><button onclick=\"alert('Answer: "+a+"')\">Show Answer</button>"
+
+list.appendChild(div)
+
+}
